82e91e18e4713961c13d147dd75b77a091a269de
Tommy Keys <tkeys2112@gmail.com>
2026-01-22 20:47:48 +0900

    fixed custom node glitch

--- addons/CustomGizmos/custom_node.gd
+++ addons/CustomGizmos/custom_node.gd
@@ -3,14 +3,16 @@ extends Node
 class_name CustomNode
 
 func _ready():
-    if not Engine.is_editor_hint():
-        return
-    var body := StaticBody3D.new()
-    body.input_ray_pickable = true
-    var shape := CollisionShape3D.new()
-    shape.shape = SphereShape3D.new()
-    shape.shape.radius = 1
-    add_child(body)
-    body.add_child(shape)
-    body.owner = get_tree().edited_scene_root
-    shape.owner = get_tree().edited_scene_root
+	if not Engine.is_editor_hint():
+		return
+	for child in get_children():
+		child.queue_free()
+	var body := StaticBody3D.new()
+	body.input_ray_pickable = true
+	var shape := CollisionShape3D.new()
+	shape.shape = SphereShape3D.new()
+	shape.shape.radius = 1
+	add_child(body)
+	body.add_child(shape)
+	body.owner = get_tree().edited_scene_root
+	shape.owner = get_tree().edited_scene_root
